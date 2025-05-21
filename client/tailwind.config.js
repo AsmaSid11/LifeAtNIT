1967264e0ad356091d7d8c5d54e4171257fe35a9
Asma <aa3corner@gmail.com>
2025-05-21 14:58:54 +0530

    Update global Styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#1F2647',     // Space Cadet - navy blue
+        lightblue: '#88DADA', // Tiffany Blue - light blue
+        teal: '#0D9488',      // Dark Cyan - teal
+        yellow: '#DDA853',    // Earth Yellow - mustard
+        snow: '#FFFBFC',  // Snow - white
+      },
+    },
   },
   plugins: [],
-}
+};
